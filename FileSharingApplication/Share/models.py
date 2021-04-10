c01d1b5b4895164084d7427eed7a2876135bb49f
Bhavesh <bhavesh189kumar@gmail.com>
2021-04-10 23:29:31 +0530

    Completed uploading part of sharing a file

--- FileSharingApplication/Share/models.py
+++ FileSharingApplication/Share/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Fileinfo(models.Model):
+    title = models.CharField(max_length=50)
+    filepath = models.FileField(upload_to='Media/',null=True,verbose_name="")
+    description = models.CharField(max_length=150)
+
+    def __str__(self) -> str:
+        return self.title
--- FileSharingApplication/Share/views.py
+++ FileSharingApplication/Share/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Fileinfo
 # Create your views here.
-def index(request):
-    return render(request,'share/index.html')
+def Share(request):
+    if request.method == "POST":
+        title = request.POST['Title']
+        file = request.FILES['myfile']
+        description = request.POST['Description']
+        Fileinfo.objects.create(filepath = file,title=title,description=description)
+        return render(request,'share/index.html')
+    else:
+        return render(request,'share/index.html')
+
+        
